dd881409e634a8c818f66b162c95e3ba8c5a0f7f
Teltel104 <plo.koon.4869@gmail.com>
2025-10-20 19:22:41 +0900

    プレイヤーの階層構造の変更

--- Assets/scripts/Skill.cs
+++ Assets/scripts/Skill.cs
@@ -2,71 +2,70 @@ using UnityEngine;
 
 public class Skill : MonoBehaviour
 {
+    [Header("以上の角度")]
+    [SerializeField] private float largeRt = 180f;
 
+    [Header("以下の角度")]
+    [SerializeField] private float smallRt = 30f;
 
-        [Header("以上の角度")]
-        [SerializeField] private float largeRt = 180f;
+    private bool isGrounded = true;      // 接地しているかどうかのフラグ
+    private float TakeoffRt;     // 離陸した瞬間のプレイヤーのZ角度
+    Rigidbody2D parentRb;
 
-        [Header("以下の角度")]
-        [SerializeField] private float smallRt = 30f;
+    private void Start()
+    {
+        parentRb = GetComponentInParent<Rigidbody2D>();
+    }
 
-        private bool isGrounded = true;      // 接地しているかどうかのフラグ
-        private float TakeoffRt;     // 離陸した瞬間のプレイヤーのZ角度
-        private Rigidbody2D rb;
+    // 他のコライダーに接触したとき
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("あああ" + col.gameObject);
+        // タグが"Ground"かつ、空中にいたときか
 
-        private void Start()
+        if (col.gameObject.CompareTag("Ground") && !isGrounded)
         {
-             rb = GetComponent<Rigidbody2D>();
-        }
+            isGrounded = true;
 
-        // 他のコライダーに接触したとき
-        private void OnTriggerEnter2D(Collider2D col)
-        {
-            // タグが"Ground"かつ、空中にいたときか
-            if (col.gameObject.CompareTag("Ground") && !isGrounded)
+            float AirRt = Mathf.Abs(Mathf.DeltaAngle(parentRb.rotation, TakeoffRt));  // Mathf.DeltaAnglseで現在の角度とジャンプした時の角度を差を出し、Mathf.Absで絶対値にする。
+
+            Debug.Log($"着地！ 空中での総回転角度: {AirRt:F2}°");
+
+            // 1. 前回の着地から次の着地までに〇°以上回転
+            if (AirRt >= largeRt)
             {
-                isGrounded = true;
-
-                float AirRt = Mathf.Abs(Mathf.DeltaAngle(rb.rotation, TakeoffRt));  // Mathf.DeltaAnglseで現在の角度とジャンプした時の角度を差を出し、Mathf.Absで絶対値にする。
-
-                Debug.Log($"着地！ 空中での総回転角度: {AirRt:F2}°");
-
-                // 1. 前回の着地から次の着地までに〇°以上回転
-                if (AirRt >= largeRt)
-                {
-                    // Debug.Log($"条件達成: {largeRt}°以上の回転！");
-                    KillScore.rotateLevel ++;
-                }
-
-                // 2. 前回の着地から次の着地までに〇°以下回転
-                if (AirRt <= smallRt)
-                {
-                    // Debug.Log($"条件達成: {smallRt}°以下の回転でした。");
-                }
-
-                // 3. 前回の着地から次の着地までの回転が85~95°以内
-                if (AirRt >= 85f && AirRt <= 95f)
-                {
-                    //Debug.Log("条件達成: 85°～95°");
-                }
-
-                AirRt = 0f;
+                // Debug.Log($"条件達成: {largeRt}°以上の回転！");
+                KillScore.rotateLevel++;
             }
-        }
 
-
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            // タグが "Ground" から離れたとき
-            if (collision.gameObject.CompareTag("Ground"))
+            // 2. 前回の着地から次の着地までに〇°以下回転
+            if (AirRt <= smallRt)
             {
-                // 接地状態から離れた瞬間（離陸の瞬間）の角度を記録
-                if (isGrounded)
-                {
-                    TakeoffRt = rb.rotation;
-                    // Debug.Log($"離陸！ この瞬間の角度を記録: {TakeoffRt:F2}°");
-                }
-                isGrounded = false; // 空中状態に更新
+                // Debug.Log($"条件達成: {smallRt}°以下の回転でした。");
             }
+
+            // 3. 前回の着地から次の着地までの回転が85~95°以内
+            if (AirRt >= 85f && AirRt <= 95f)
+            {
+                //Debug.Log("条件達成: 85°～95°");
+            }
+
+            AirRt = 0f;
         }
+
+    }
+     private void OnTriggerExit2D(Collider2D collision)
+     {
+        // タグが "Ground" から離れたとき
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            // 接地状態から離れた瞬間（離陸の瞬間）の角度を記録
+            if (isGrounded)
+            {
+                TakeoffRt = parentRb.rotation;
+                // Debug.Log($"離陸！ この瞬間の角度を記録: {TakeoffRt:F2}°");
+            }
+            isGrounded = false; // 空中状態に更新
+        }
+     }
 }
