0e3cff13ae2ec43184ade01c9c63b035cdbc7137
Teltel104 <plo.koon.4869@gmail.com>
2025-10-27 14:33:53 +0900

    プレイヤースクリプトの名前修正

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
 
-    public CumulativeRotationTracker player;
+    public PlayerC player;
  
      private void Awake()
      {
