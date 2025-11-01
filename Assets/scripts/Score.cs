f7fce55fc6ced6e8b0e8e4bdc5ca83bcdad26afd
butanoriwo <norinoriwo@gmail.com>
2025-11-01 16:26:43 +0900

    自動ランキングとスコア計算の実装

--- Assets/scripts/Score.cs
+++ Assets/scripts/Score.cs
@@ -4,26 +4,32 @@ using UnityEngine;
 
 public static class KillScore
 {
-    public static int killScore = 0;
     public static int Level = 0;
     public static float rotateLevel;
     public static int damageLevel;
     public static int jumpLevel;
     public static bool barrier;
 
-
+    public static int killScore = 0;
+    public static int timeScore = 0;
+    public static int highScore = 0;
     public static int LifeOrBullet = 10;
     public static int Combo = 0;
 
+    public static int first = 0;
+    public static int second = 0;
+    public static int third = 0;
+
     public static void Reset()
     {
-        killScore = 0;
         Level = 0;
         rotateLevel = 1f;
         damageLevel = 1;
         jumpLevel = 1;
         barrier = false;
 
+        killScore = 0;
+        timeScore = 0;
         LifeOrBullet = 10;
         Combo = 0;
     }
