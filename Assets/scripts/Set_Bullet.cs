6ab38b727c8ed8a3205aab832028ea44b16151ad
Teltel104 <plo.koon.4869@gmail.com>
2025-11-01 14:30:34 +0900

    コンボのプログラムを追加

--- Assets/scripts/Set_Bullet.cs
+++ Assets/scripts/Set_Bullet.cs
@@ -4,17 +4,18 @@ public class Set_Bullet : MonoBehaviour
 {
     public bool common = false;
 
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
             KillScore.Combo++;
+
+            if (Combo.Instance != null)
+            {
+                Combo.Instance.ShowCombo(KillScore.Combo);
+            }
+
             if (common)
             {
                 Destroy(gameObject);
@@ -23,11 +24,15 @@ public class Set_Bullet : MonoBehaviour
         }
         else if (collision.gameObject.CompareTag("Ground"))
         {
-            if (common) {
+            if (common) 
+            {
                 KillScore.ComboReset();
+
+                if (Combo.Instance != null)
+                {
+                    Combo.Instance.HideCombo();
+                }
             }
         }
-
-
     }
 }
