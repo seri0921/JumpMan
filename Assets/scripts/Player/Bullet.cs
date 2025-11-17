896f477765ec1307c4137c3d5913407ce2a4566f
butanoriwo <norinoriwo@gmail.com>
2025-11-17 16:17:21 +0900

    デバッグ

--- Assets/scripts/Player/Bullet.cs
+++ Assets/scripts/Player/Bullet.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Net.NetworkInformation;
-using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
--- Assets/scripts/UI/SelectTextLight.cs
+++ Assets/scripts/UI/SelectTextLight.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
-using UnityEditor.ShaderGraph;
+
 
 public class SelectTextLight : MonoBehaviour
 {
